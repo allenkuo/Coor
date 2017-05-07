c98bf60b54f8afca4e48cb1ad69e345c42fc9ca8
allenkuo <weicheu@gmail.com>
2017-05-07 16:19:51 +0800

    first cimmit
    first commit

---
+++ app/src/main/java/com/example/allen/coor/BottomBarBehavior.java
@@ -0,0 +1,40 @@
+package com.example.allen.coor;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Allen on 2017/5/7.
+ *
+ */
+
+public class BottomBarBehavior extends CoordinatorLayout.Behavior<View> {
+
+    public static final String TAG = BottomBarBehavior.class.getSimpleName();
+    private int deltaY;
+
+    public BottomBarBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
+        deltaY += dy;
+        if (deltaY < 0) {
+            deltaY = 0;
+        } else if (deltaY > child.getHeight()) {
+            deltaY = child.getHeight();
+        }
+        child.setTranslationY(deltaY);
+    }
+
+}
