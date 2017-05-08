171f3d13ed5995b28c54db9f13a44cdb09e1214a
allenkuo <weicheu@gmail.com>
2017-05-08 21:49:27 +0800

    modify
    modify

--- app/src/main/java/com/example/allen/coor/BottomBarBehavior.java
+++ app/src/main/java/com/example/allen/coor/BottomBarBehavior.java
@@ -1,8 +1,8 @@
 package com.example.allen.coor;
 
 import android.content.Context;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -14,27 +14,39 @@ import android.view.View;
 public class BottomBarBehavior extends CoordinatorLayout.Behavior<View> {
 
     public static final String TAG = BottomBarBehavior.class.getSimpleName();
-    private int deltaY;
+//    private int deltaY;
 
     public BottomBarBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+//    @Override
+//    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+//        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+//    }
+
+//    @Override
+//    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
+//        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
+//        deltaY += dy;
+//        if (deltaY < 0) {
+//            deltaY = 0;
+//        } else if (deltaY > child.getHeight()) {
+//            deltaY = child.getHeight();
+//        }
+//        child.setTranslationY(deltaY);
+//    }
+
+
     @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
-        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof AppBarLayout;
     }
 
     @Override
-    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
-        deltaY += dy;
-        if (deltaY < 0) {
-            deltaY = 0;
-        } else if (deltaY > child.getHeight()) {
-            deltaY = child.getHeight();
-        }
-        child.setTranslationY(deltaY);
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        float translationY = Math.abs(dependency.getTop());
+        child.setTranslationY(translationY);
+        return true;
     }
-
 }
